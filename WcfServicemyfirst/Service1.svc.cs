f24439343676fe3cb45f6f928e7317d170a74fb2
mura0149 <31722880+mura0149@users.noreply.github.com>
2018-02-12 10:26:13 +0100

    GetMyName

--- WcfServicemyfirst/Service1.svc.cs
+++ WcfServicemyfirst/Service1.svc.cs
@@ -12,22 +12,9 @@ namespace WcfServicemyfirst
     // NOTE: In order to launch WCF Test Client for testing this service, please select Service1.svc or Service1.svc.cs at the Solution Explorer and start debugging.
     public class Service1 : IService1
     {
-        public string GetData(int value)
+        public string getMyName()
         {
-            return string.Format("You entered: {0}", value);
-        }
-
-        public CompositeType GetDataUsingDataContract(CompositeType composite)
-        {
-            if (composite == null)
-            {
-                throw new ArgumentNullException("composite");
-            }
-            if (composite.BoolValue)
-            {
-                composite.StringValue += "Suffix";
-            }
-            return composite;
+            return "Murat";
         }
     }
 }
